482c49bde85ef46c7ea0cf1f07c29c9ea796a4b6
Spyridon Polemis <jc938116@ad.jcu.edu.au>
2025-09-14 14:10:56 +1000

    Move getInfo function from Book constructor to prototype

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ function Book(title, author, pages, haveRead) {
     this.haveRead = haveRead;
     this.id = crypto.randomUUID()
 
-    // TODO: Move this to prototype
-    this.getInfo = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${haveRead ? "have read" : "have not read"}.`
-    } 
 }
+
+Book.prototype.getInfo = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${haveRead ? "have read" : "have not read"}.`
+} 
 
 function addBookToLibrary(title, author, pages, hasRead) {
     // Creates a book object and stores it in myLibrary array
     myLibrary.push(new Book(title, author, pages, hasRead))
 }
+
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
