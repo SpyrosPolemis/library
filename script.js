0c3ce1a6777b0e097cca8ae0708e3c14392a63e9
Spyridon Polemis <jc938116@ad.jcu.edu.au>
2025-09-14 14:06:38 +1000

    Create addBookToLibrary function

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+const myLibrary = [];
+
 function Book(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.haveRead = haveRead;
+    this.id = crypto.randomUUID()
+
+    // TODO: Move this to prototype
     this.getInfo = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${haveRead ? "have read" : "have not read"}.`
     } 
+}
+
+function addBookToLibrary(title, author, pages, hasRead) {
+    // Creates a book object and stores it in myLibrary array
+    myLibrary.push(new Book(title, author, pages, hasRead))
 }
